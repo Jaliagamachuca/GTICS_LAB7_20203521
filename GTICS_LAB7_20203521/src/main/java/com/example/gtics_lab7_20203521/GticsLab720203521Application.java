51ffe579ab29d6f4202584344ecb1e16b71ffa5a
Julio Cesar <a20203521@pucp.edu.pe>
2024-05-29 19:47:17 -0500

    creacion del documento

---
+++ GTICS_LAB7_20203521/src/main/java/com/example/gtics_lab7_20203521/GticsLab720203521Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab7_20203521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab720203521Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab720203521Application.class, args);
+    }
+
+}
---
+++ GTICS_LAB7_20203521/src/test/java/com/example/gtics_lab7_20203521/GticsLab720203521ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab7_20203521;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab720203521ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
